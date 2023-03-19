b59de77be85b77bd4d7f936677cb22940cecff49
harsh <harshsharma311003@gmail.com>
2023-03-20 00:11:15 +0530

    added redirect back after loggin in from where it was called

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import Base from "./Base";
 import Map from "../components/Map";
-import { API } from "../config/backend";
-import socketioclient from "socket.io-client/dist/socket.io";
 
 const Home = () => {
   // states
   const [lng, setLng] = useState(80.9462);
   const [lat, setLat] = useState(26.8467);
   const [zoom, setZoom] = useState(16);
-  const socket = socketioclient(API);
 
   //functions
 
@@ -71,7 +68,6 @@ const Home = () => {
             setLat={setLat}
             zoom={zoom}
             setZoom={setZoom}
-            socket={socket}
           />
           {/* map component end */}
 
